d8f7dda693432ffbcc0f8cf8f63cb56355943d87
Dima Ostanin <xnadab@gmail.com>
2022-10-24 22:52:24 +0300

    HW10 after V.R cheak

--- front end/html-css-js-introduction/src/index.js
+++ front end/html-css-js-introduction/src/index.js
@@ -27,8 +27,9 @@ function setDetails(anchor) {
 for(let i = 0;i < anchors.length;i++) {
     anchors[i].addEventListener("click",function(){
         console.log("event - click on ", anchors[i]);
-        showDetails();
         setDetails(anchors[i]);
+        showDetails();
+        
     })
 }
 function showDetails() {
@@ -40,4 +41,5 @@ function showDetails() {
 }
 function hideDetails() {
     mainClass.classList.add(HIDDEN);
+    pauseAudio();
 }
