7b830dc84b54ab7d900511967154d534b1d98c28
Dima Ostanin <xnadab@gmail.com>
2022-10-24 20:31:06 +0300

    HW10  ready

--- front end/html-css-js-introduction/src/index.js
+++ front end/html-css-js-introduction/src/index.js
@@ -2,11 +2,15 @@ console.log("Launching script");
 const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+const detailsAudio = document.querySelector(".details-audio");
 const mainClass = document.querySelector(".main-class");
+const detailsContainer = document.querySelector(".details-container");
 const HIDDEN = "hidden";
+const IS_POINT = "is-point";
 function setDetails(anchor) { 
     detailsImage.setAttribute("src" ,anchor.getAttribute("data-details-image"));
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
+    detailsAudio.setAttribute("src" ,anchor.getAttribute("data-details-audio"));
 
 }
 
@@ -19,6 +23,10 @@ for(let i = 0;i < anchors.length;i++) {
 }
 function showDetails() {
     mainClass.classList.remove(HIDDEN);
+    detailsContainer.classList.add(IS_POINT);
+    setTimeout(function() {
+        detailsContainer.classList.remove(IS_POINT);
+    },1);
 }
 function hideDetails() {
     mainClass.classList.add(HIDDEN);
