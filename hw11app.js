aa1d2ad07686162287850689c8cc2e8157091ae8
Dima Ostanin <xnadab@gmail.com>
2022-10-28 00:37:56 +0300

    hw11 with while after V.R check

--- hw11app.js
+++ hw11app.js
@@ -1,15 +1,29 @@
 
+// function sumDigits(number) {
+//    if (number < 0)
+// 		number = -number;
+//     let sum;
+//          for( sum = 0 ,rem = 0;number > 0;  )   {
+
+//             rem = number % 10;
+//             sum = sum + rem;
+//             number = (number - rem) / 10;
+            
+//          }
+//        return sum;
+// }
+console.log("sum of digits" ,sumDigits(-12345));
+
 function sumDigits(number) {
    if (number < 0)
 		number = -number;
-   // let sum;
-         for( sum = 0 ,rem = 0;number > 0;  )   {
 
-            rem = number % 10;
-            sum = sum + rem;
-            number = (number - rem) / 10;
-            
-         }
-       return sum;
+   let sum = 0,rem = 0;
+
+   while (number) {
+       rem = number % 10;
+       sum += rem;
+       number = (number - rem) / 10;
+   }
+   return sum;
 }
-console.log("sum of digits" ,sumDigits(-12345));
