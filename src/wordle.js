9cdf8bf69bb278ad145ce935acffce36b9b786e3
Dima Ostanin <xnadab@gmail.com>
2022-11-14 22:30:02 +0200

    hw16 v2

--- src/wordle.js
+++ src/wordle.js
@@ -1,22 +1,27 @@
-const word = "table";
+const word = "apple";
 const N_LETTERS = 5;
 const letterElements = document.querySelectorAll(".letter-guess")
 var trials = 2;
+var trialsUsed = 0;
 const cong = document.querySelector(".message-congs")
 const HIDDEN = "hidden";
 const fail = document.querySelector(".message-fail")
 const HIDDEN2 = "hidden2";
 let counter = document.querySelector('.counter-display');
+const scounterUsed = document.querySelector('.counter-display-used');
 
 function onChange(event) {
-     counter.innerHTML = trials;
+    
+    counter.innerHTML = 'Left trials' + ' ' +trials;
     const wordGuess = event.target.value;
     event.target.value = '';
     if(wordGuess.length != N_LETTERS ) {
        alert(`A word should contain ${N_LETTERS} letters`)
-       trials--;
+       counterPlus()
        if(trials == -1){
         fail.classList.remove(HIDDEN2);
+        restart();
+        
     }
     }  else  {
        const wordAr = Array.from(wordGuess);
@@ -25,20 +30,36 @@ function onChange(event) {
         let index = word.indexOf(l);
         let res = 'red';
         if (index > - 1) {
-            res = index == i ? 'green' : 'yellow';
+            res = index == l ? 'yellow' : 'green';
         }
             return res;
        })
        colors.forEach((c,i) => letterElements[i].style.color=c)
        if (wordGuess == word) {
-        cong.classList.remove(HIDDEN)
+        counterPlus()
+        cong.classList.remove(HIDDEN);
+        restart();
       } else  {
-        trials--;
-        
+        counterPlus()
         if( trials == -1){
             fail.classList.remove(HIDDEN2);
+            restart();
         }
     }
 }
     }
     
+    function counterPlus() {
+        trials--;
+        trialsUsed++;
+        scounterUsed.innerHTML = 'Trial used' + ' :' + trialsUsed;
+    }
+    function restart(){
+         trials = 2;
+         trialsUsed = 0;
+         setTimeout(() => {
+            cong.classList.add(HIDDEN);
+            fail.classList.add(HIDDEN2);
+         }, 4000);
+         
+    }
