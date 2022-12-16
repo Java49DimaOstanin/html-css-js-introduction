7d6461bee41beb17813108a6af6e258702d5659a
DimaOstanin <xnadab@gmail.com>
2022-12-16 15:47:02 +0200

    new stage half refactored

--- src/main.js
+++ src/main.js
@@ -1,37 +1,40 @@
 import { Company } from "./data/company.js";
 import { EmployeeForm } from "./ui/employeeForm.js";
-import { showErrorMessage } from "./ui/errorMessage.js";
+import { EmployeesList } from "./ui/EmployeeList.js";
+
+import { SalariesForm } from "./ui/SalariesForm.js";
 
 const MIN_SALARY = 50;
 const MAX_SALARY = 2000;
 const MIN_YEAR = new Date ('1980-01-01')
-
-
 const ACTIVE = "active"
 
-
-
-
-const salaryFormErrorElement = document.getElementById("salary_form_error");
-const employeesListElement = document.getElementById("employees-all");
-const employeesSalaryListElement = document.getElementById("employees-salary");
+const listAllEmployees = new EmployeesList("employees-all");
+const listEmployeesBySalary = new EmployeesList("employees-salary")
 const sectionsElement = document.querySelectorAll("section");
 const buttonsMenuElement = document.querySelectorAll(".buttons-menu *");
 const bookListelement = document.getElementById("books-by-author")
 const authorInputElements = document.querySelectorAll(".author-form-class  [name]")
 /************************************************************************** */
-//functions of Company
+
 
 
 const company = new Company();
-//functions of Employee Form
 
+const paramSalaries = { idForm: "salary-form" ,idSalaryInput: "salaryFrom",
+idSalaryToInput: "salaryTo" ,idErrorMessage: "salary_form_error" }
+const salariesForm = new SalariesForm(paramSalaries);
+salariesForm.addSubmitHandler((salariesObj) => {
+    const employee = company.getEmployeesBySalary(salariesObj.salaryFrom, 
+        salariesObj.salaryTo);
+    listEmployeesBySalary.showEmployees(employee);
+})
 
 
 
 const employeeForm = new EmployeeForm({idForm: "employee_form", idDateInput: "date_input",
-idSalaryInput: "salary_input", idDateError: "date_error", idSalaryError: "salary_error",
- minYear: MIN_YEAR, minSalary: MIN_SALARY, maxSalary: MAX_SALARY})
+       idSalaryInput: "salary_input", idDateError: "date_error", idSalaryError: "salary_error",
+       minYear: MIN_YEAR, minSalary: MIN_SALARY, maxSalary: MAX_SALARY})
  employeeForm.addSubmitHandler((employee) => company.hireEmployee(employee))
 /************************************************************* */
 function onSubmitAuthor(event) {
@@ -42,32 +45,7 @@ function onSubmitAuthor(event) {
 }
 /********************************************************************************** */
 
-//functions of Salary Form
 
-let salaryFrom = 0;
-let salaryTo = 0;
-function onSubmitSalary(event) {
-    event.preventDefault();
-    const employees = company.getEmployeesBySalary(salaryFrom, salaryTo);
-    employeesSalaryListElement.innerHTML = getEmployeeItems(employees); 
-}
-function onChangeSalaryFrom(event) {
-    const value = +event.target.value;
-    if (salaryTo && value >= salaryTo) {
-        showErrorMessage(event.target, "Salary 'from' must be less than Salary 'to'",
-        salaryFormErrorElement);
-    } else {
-        salaryFrom = value;
-    }
-}
-function onChangeSalaryTo(event) {
-    const value = +event.target.value;
-    if (salaryFrom && value < salaryFrom) {
-        showErrorMessage(event.target, "Salary 'To' must be greater than Salary 'From'",
-        salaryFormErrorElement);
-    }
-    salaryTo = value;
-}
 function showSection(index) {
     buttonsMenuElement.forEach(e => e.classList.remove(ACTIVE));
     sectionsElement.forEach(e => e.hidden = true)
@@ -75,25 +53,12 @@ function showSection(index) {
     sectionsElement[index].hidden = false;
     if (index == 1) {
         const employees = company.getAllEmployees();
-        employeesListElement.innerHTML = getEmployeeItems(employees);
+        listAllEmployees.showEmployees(employees);
     }
 }
-function getEmployeeItems(employees) {
-    return employees.map (e => 
-        `<li class="employees-item">
-              <div class="employees-item-container">
-                 <p class="employees-item-paragraph">Author name: ${e.employee_name} </p>
-                 <p class="employees-item-paragraph">Book name: ${e.email} </p>
-                 <p class="employees-item-paragraph">Genre: ${e.department}</p>
-                 <p class="employees-item-paragraph">Publish date: ${e.birthDate}</p>
-                 <p class="employees-item-paragraph">Pages: ${e.salary}</p>
-              </div>
-          </li>`).join('');
-}
+
 
 
 window.showSection = showSection;
-window.onChangeSalaryTo = onChangeSalaryTo;
-window.onChangeSalaryFrom = onChangeSalaryFrom
-window.onSubmitSalary = onSubmitSalary
+
 window.onSubmitAuthor = onSubmitAuthor
